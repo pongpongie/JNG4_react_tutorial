2969718c8f53ad2f05a330c319cab22f90ced08e
pongpongie <arevolvera@gmail.com>
2024-04-09 17:10:56 +0900

    feat: implement TicTacToe
    TicTacToe.js
    - implemented functions to show "O", "X" to user
    - still working on, now on chapter Tutorial: Tic-Tac-Toe's 'Taking turns'
    
    styles.css
    - modified board style
    
    index.js
    - made <App/> unused
    - styled fixed by eslint, prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./styles.css";
+import App from "./App";
+import TicTacToe from "./TicTacToe";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <TicTacToe />
   </React.StrictMode>
 );
 
